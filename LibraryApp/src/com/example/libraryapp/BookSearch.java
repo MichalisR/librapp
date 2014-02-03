5903842d779088601c9fa6bf27d5485bb0ba6ccd
Michalis Rodios <michalis.rodios@gmail.com>
2014-02-03 15:37:45 +0200

    Search added

--- LibraryApp/src/com/example/libraryapp/BookSearch.java
+++ LibraryApp/src/com/example/libraryapp/BookSearch.java
@@ -2,16 +2,26 @@ package com.example.libraryapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
-import android.view.Window;
+import android.widget.Toast;
 
 public class BookSearch extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_book_search);
+		
+		// Get the intent, verify the action and get the query
+	    Intent intent = getIntent();
+	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+	      String query = intent.getStringExtra(SearchManager.QUERY);
+	      Log.i("TOAST", query);
+	      //doMySearch(query);
+	    }
 	}
 
 	@Override
